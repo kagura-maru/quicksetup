94f6adbde1f1fe6b498a7c0613e8530506eff297
kaguramaru <92495243+kagura-maru@users.noreply.github.com>
2024-11-26 16:19:28 +0800

    Update qs.sh
    Added file exist checker

--- qs.sh
+++ qs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-# v0.02
+# v0.03
 
 #### Installing
 # update and download apt
@@ -20,108 +20,119 @@ autorecon_installation () {
 }
 # zsh
 zsh_installation() {
-	echo "\033[34m[!] Installing ZSH and Plugins \033[0m"
-	sh -c "$(wget -q https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-	echo "\033[32m[+] ZSH Installed \033[0m" 
-	git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/kali/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/kali/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-	echo "plugins=(git zsh-syntax-highlighting zsh-autosuggestions )" >> /home/kali/.zshrc
-	echo "source /home/kali/.oh-my-zsh/oh-my-zsh.sh" >> /home/kali/.zshrc
+	if [ ! -d '$HOME/.zshrc' ]; then	
+		echo "\033[34m[!] Installing ZSH and Plugins \033[0m"
+		sh -c "$(wget -q https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+		echo "\033[32m[+] ZSH Installed \033[0m" 
+		git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+		echo "plugins=(git zsh-syntax-highlighting zsh-autosuggestions )" >> $HOME/.zshrc
+		echo "source $HOME/.oh-my-zsh/oh-my-zsh.sh" >> $HOME/.zshrc
+	else
+		echo "\033[34m[!] ZSH is already installed.\033[0m"
+	fi
 }
 
 # nerdfont
 nerdfont_installation() {
-	echo "\033[34m[!] Installing nerdfont \033[0m"
-	wget -q -P /home/kali/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
-	unzip /home/kali/.local/share/fonts/JetBrainsMono.zip -d /home/kali/.local/share/fonts/
-	fc-cache -f /home/kali/.local/share/fonts/
-	echo "\033[32m[+] nerdfont installed \033[0m"
+	if [ ! -d '$HOME/.local/share/fonts/JetBrainsMonoNerdFont-Medium.ttf' ]; then	
+		echo "\033[34m[!] Installing nerdfont \033[0m"
+		wget -q -P $HOME/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+		unzip $HOME/.local/share/fonts/JetBrainsMono.zip -d $HOME/.local/share/fonts/
+		fc-cache -f $HOME/.local/share/fonts/
+		echo "\033[32m[+] nerdfont installed \033[0m"
+	else
+		echo "\033[34m[!] NerdFont is already installed.\033[0m"
+	fi
 }
 
 # go
 go_installation () {
 	echo "\033[34m[!] Installing Go \033[0m"
-	sh -c "(wget -q https://go.dev/dl/go1.23.3.linux-amd64.tar.gz -O /home/kali/Downloads/go1.23.3.linux-amd64.tar.gz)"
-	sudo rm -rf /usr/local/go && tar -C /usr/local -xzf /home/kali/Downloads/go1.23.3.linux-amd64.tar.gz
+	sh -c "(wget -q https://go.dev/dl/go1.23.3.linux-amd64.tar.gz -O $HOME/Downloads/go1.23.3.linux-amd64.tar.gz)"
+	sudo rm -rf /usr/local/go && tar -C /usr/local -xzf $HOME/Downloads/go1.23.3.linux-amd64.tar.gz
 	export PATH="$PATH:/usr/local/go/bin"
 	echo "\033[32m[+] Go Installed and Path is set"
 }
 
 # catpuccin setting
 catpuccin_installation () {
+	terminator_config="$terminator_config"
 	echo "\033[34m[!] Installing Terminator catpuccin\033[0m"
-	mkdir /home/kali/.config/terminator
-	touch /home/kali/.config/terminator/config
-	echo  '[global_config]' > /home/kali/.config/terminator/config
-	echo  '[keybindings]' >> /home/kali/.config/terminator/config
-	echo  '[profiles]' >> /home/kali/.config/terminator/config
-	echo  '  [[default]]' >> /home/kali/.config/terminator/config
-	echo  '    cursor_color = "#f5e0dc"' >> /home/kali/.config/terminator/config
-	echo  '    background_color = "#1e1e2e"' >> /home/kali/.config/terminator/config
-	echo  '    foreground_color = "#cdd6f4"' >> /home/kali/.config/terminator/config
-	echo  '    background_darkness = 0.95' >> /home/kali/.config/terminator/config
-	echo  '    background_type = transparent' >> /home/kali/.config/terminator/config
-	echo  '    show_titlebar = False' >> /home/kali/.config/terminator/config
-	echo  '    scrollback_infinite = True' >> /home/kali/.config/terminator/config
-	echo  '    palette = "#45475a:#f38ba8:#a6e3a1:#f9e2af:#89b4fa:#f5c2e7:#94e2d5:#bac2de:#585b70:#f38ba8:#a6e3a1:#f9e2af:#89b4fa:#f5c2e7:#94e2d5:#a6adc8"' >> /home/kali/.config/terminator/config 
-	echo  '    background_image = None' >> /home/kali/.config/terminator/config
-	echo  '  [[Catppuccin_Latte]]' >> /home/kali/.config/terminator/config
-	echo  '    cursor_color = "#dc8a78"' >> /home/kali/.config/terminator/config
-	echo  '    background_color = "#eff1f5"' >> /home/kali/.config/terminator/config
-	echo  '    foreground_color = "#4c4f69"' >> /home/kali/.config/terminator/config
-	echo  '    palette = "#5c5f77:#d20f39:#40a02b:#df8e1d:#1e66f5:#ea76cb:#179299:#acb0be:#6c6f85:#d20f39:#40a02b:#df8e1d:#1e66f5:#ea76cb:#179299:#bcc0cc"' >> /home/kali/.config/terminator/config 
-	echo  '    background_image = None' >> /home/kali/.config/terminator/config
-	echo  '  [[Catppuccin_Frappe]]' >> /home/kali/.config/terminator/config
-	echo  '    cursor_color = "#f2d5cf"' >> /home/kali/.config/terminator/config
-	echo  '    background_color = "#303446"' >> /home/kali/.config/terminator/config
-	echo  '    foreground_color = "#c6d0f5"' >> /home/kali/.config/terminator/config
-	echo  '    palette = "#51576d:#e78284:#a6d189:#e5c890:#8caaee:#f4b8e4:#81c8be:#b5bfe2:#626880:#e78284:#a6d189:#e5c890:#8caaee:#f4b8e4:#81c8be:#a5adce"' >> /home/kali/.config/terminator/config 
-	echo  '    background_image = None' >> /home/kali/.config/terminator/config
-	echo  '  [[Catppuccin_Macchiato]]' >> /home/kali/.config/terminator/config
-	echo  '    cursor_color = "#f4dbd6"' >> /home/kali/.config/terminator/config
-	echo  '    background_color = "#24273a"' >> /home/kali/.config/terminator/config
-	echo  '    foreground_color = "#cad3f5"' >> /home/kali/.config/terminator/config
-	echo  '    palette = "#494d64:#ed8796:#a6da95:#eed49f:#8aadf4:#f5bde6:#8bd5ca:#b8c0e0:#5b6078:#ed8796:#a6da95:#eed49f:#8aadf4:#f5bde6:#8bd5ca:#a5adcb"' >> /home/kali/.config/terminator/config 
-	echo  '    background_image = None' >> /home/kali/.config/terminator/config
-	echo  '  [[Catppuccin_Mocha]]' >> /home/kali/.config/terminator/config
-	echo  '    cursor_color = "#f5e0dc"' >> /home/kali/.config/terminator/config
-	echo  '    background_color = "#1e1e2e"' >> /home/kali/.config/terminator/config
-	echo  '    foreground_color = "#cdd6f4"' >> /home/kali/.config/terminator/config
-	echo  '    palette = "#45475a:#f38ba8:#a6e3a1:#f9e2af:#89b4fa:#f5c2e7:#94e2d5:#bac2de:#585b70:#f38ba8:#a6e3a1:#f9e2af:#89b4fa:#f5c2e7:#94e2d5:#a6adc8"' >> /home/kali/.config/terminator/config 
-	echo  '    background_image = None' >> /home/kali/.config/terminator/config
-	echo  '[layouts]' >> /home/kali/.config/terminator/config
-	echo  '  [[default]]' >> /home/kali/.config/terminator/config
-	echo  '    [[[window0]]]' >> /home/kali/.config/terminator/config
-	echo  '      type = Window' >> /home/kali/.config/terminator/config
-	echo  '      parent = ""' >> /home/kali/.config/terminator/config
-	echo  '    [[[child1]]]' >> /home/kali/.config/terminator/config
-	echo  '      type = Terminal' >> /home/kali/.config/terminator/config
-	echo  '      parent = window0' >> /home/kali/.config/terminator/config
-	echo  '[plugins]' >> /home/kali/.config/terminator/config
+	mkdir $HOME/.config/terminator
+	touch $HOME/$terminator_config
+	cp $HOME/$terminator_config $HOME/$terminator_config.bak
+	echo  '[global_config]' > $HOME/$terminator_config
+	echo  '[keybindings]' >> $HOME/$terminator_config
+	echo  '[profiles]' >> $HOME/$terminator_config
+	echo  '  [[default]]' >> $HOME/$terminator_config
+	echo  '    cursor_color = "#f5e0dc"' >> $HOME/$terminator_config
+	echo  '    background_color = "#1e1e2e"' >> $HOME/$terminator_config
+	echo  '    foreground_color = "#cdd6f4"' >> $HOME/$terminator_config
+	echo  '    background_darkness = 0.95' >> $HOME/$terminator_config
+	echo  '    background_type = transparent' >> $HOME/$terminator_config
+	echo  '    show_titlebar = False' >> $HOME/$terminator_config
+	echo  '    scrollback_infinite = True' >> $HOME/$terminator_config
+	echo  '    palette = "#45475a:#f38ba8:#a6e3a1:#f9e2af:#89b4fa:#f5c2e7:#94e2d5:#bac2de:#585b70:#f38ba8:#a6e3a1:#f9e2af:#89b4fa:#f5c2e7:#94e2d5:#a6adc8"' >> $HOME/$terminator_config 
+	echo  '    background_image = None' >> $HOME/$terminator_config
+	echo  '  [[Catppuccin_Latte]]' >> $HOME/$terminator_config
+	echo  '    cursor_color = "#dc8a78"' >> $HOME/$terminator_config
+	echo  '    background_color = "#eff1f5"' >> $HOME/$terminator_config
+	echo  '    foreground_color = "#4c4f69"' >> $HOME/$terminator_config
+	echo  '    palette = "#5c5f77:#d20f39:#40a02b:#df8e1d:#1e66f5:#ea76cb:#179299:#acb0be:#6c6f85:#d20f39:#40a02b:#df8e1d:#1e66f5:#ea76cb:#179299:#bcc0cc"' >> $HOME/$terminator_config 
+	echo  '    background_image = None' >> $HOME/$terminator_config
+	echo  '  [[Catppuccin_Frappe]]' >> $HOME/$terminator_config
+	echo  '    cursor_color = "#f2d5cf"' >> $HOME/$terminator_config
+	echo  '    background_color = "#303446"' >> $HOME/$terminator_config
+	echo  '    foreground_color = "#c6d0f5"' >> $HOME/$terminator_config
+	echo  '    palette = "#51576d:#e78284:#a6d189:#e5c890:#8caaee:#f4b8e4:#81c8be:#b5bfe2:#626880:#e78284:#a6d189:#e5c890:#8caaee:#f4b8e4:#81c8be:#a5adce"' >> $HOME/$terminator_config 
+	echo  '    background_image = None' >> $HOME/$terminator_config
+	echo  '  [[Catppuccin_Macchiato]]' >> $HOME/$terminator_config
+	echo  '    cursor_color = "#f4dbd6"' >> $HOME/$terminator_config
+	echo  '    background_color = "#24273a"' >> $HOME/$terminator_config
+	echo  '    foreground_color = "#cad3f5"' >> $HOME/$terminator_config
+	echo  '    palette = "#494d64:#ed8796:#a6da95:#eed49f:#8aadf4:#f5bde6:#8bd5ca:#b8c0e0:#5b6078:#ed8796:#a6da95:#eed49f:#8aadf4:#f5bde6:#8bd5ca:#a5adcb"' >> $HOME/$terminator_config 
+	echo  '    background_image = None' >> $HOME/$terminator_config
+	echo  '  [[Catppuccin_Mocha]]' >> $HOME/$terminator_config
+	echo  '    cursor_color = "#f5e0dc"' >> $HOME/$terminator_config
+	echo  '    background_color = "#1e1e2e"' >> $HOME/$terminator_config
+	echo  '    foreground_color = "#cdd6f4"' >> $HOME/$terminator_config
+	echo  '    palette = "#45475a:#f38ba8:#a6e3a1:#f9e2af:#89b4fa:#f5c2e7:#94e2d5:#bac2de:#585b70:#f38ba8:#a6e3a1:#f9e2af:#89b4fa:#f5c2e7:#94e2d5:#a6adc8"' >> $HOME/$terminator_config 
+	echo  '    background_image = None' >> $HOME/$terminator_config
+	echo  '[layouts]' >> $HOME/$terminator_config
+	echo  '  [[default]]' >> $HOME/$terminator_config
+	echo  '    [[[window0]]]' >> $HOME/$terminator_config
+	echo  '      type = Window' >> $HOME/$terminator_config
+	echo  '      parent = ""' >> $HOME/$terminator_config
+	echo  '    [[[child1]]]' >> $HOME/$terminator_config
+	echo  '      type = Terminal' >> $HOME/$terminator_config
+	echo  '      parent = window0' >> $HOME/$terminator_config
+	echo  '[plugins]' >> $HOME/$terminator_config
 	echo "\033[32m[+] Terminator catpuccin set \033[0m"
 }
 
 # copyq
 copyq_installation () {
+	copyq_service="$copyq_service"
 	echo "\033[34m[!] Setting CopyQ \033[0m"
-	sudo touch /etc/systemd/system/copyq.service
-	sudo chmod 777 /etc/systemd/system/copyq.service
-	sudo echo '[Unit]' > /etc/systemd/system/copyq.service
-	sudo echo 'Description=CopyQ Clipboard Manager' >> /etc/systemd/system/copyq.service
-	sudo echo 'Documentation=man:copyq(1)' >> /etc/systemd/system/copyq.service
-	sudo echo 'After=graphical.target' >> /etc/systemd/system/copyq.service
-	sudo echo '' >> /etc/systemd/system/copyq.service
-	sudo echo '[Service]' >> /etc/systemd/system/copyq.service
-	sudo echo 'ExecStart=/usr/bin/copyq' >> /etc/systemd/system/copyq.service
-	sudo echo 'Restart=always' >> /etc/systemd/system/copyq.service
-	sudo echo 'Environment=DISPLAY=:0' >> /etc/systemd/system/copyq.service
-	sudo echo 'Environment=XAUTHORITY=/home/kali/.Xauthority' >> /etc/systemd/system/copyq.service
-	sudo echo 'User=kali' >> /etc/systemd/system/copyq.service
-	sudo echo 'Group=kali' >> /etc/systemd/system/copyq.service
-	sudo echo '' >> /etc/systemd/system/copyq.service
-	sudo echo '[Install]' >> /etc/systemd/system/copyq.service
-	sudo echo 'WantedBy=default.target' >> /etc/systemd/system/copyq.service
-	sudo chmod 755 /etc/systemd/system/copyq.service
+	sudo touch $copyq_service
+	sudo chmod 777 $copyq_service
+	sudo echo '[Unit]' > $copyq_service
+	sudo echo 'Description=CopyQ Clipboard Manager' >> $copyq_service
+	sudo echo 'Documentation=man:copyq(1)' >> $copyq_service
+	sudo echo 'After=graphical.target' >> $copyq_service
+	sudo echo '' >> $copyq_service
+	sudo echo '[Service]' >> $copyq_service
+	sudo echo 'ExecStart=/usr/bin/copyq' >> $copyq_service
+	sudo echo 'Restart=always' >> $copyq_service
+	sudo echo 'Environment=DISPLAY=:0' >> $copyq_service
+	sudo echo 'Environment=XAUTHORITY=$HOME/.Xauthority' >> $copyq_service
+	sudo echo 'User=kali' >> $copyq_service
+	sudo echo 'Group=kali' >> $copyq_service
+	sudo echo '' >> $copyq_service
+	sudo echo '[Install]' >> $copyq_service
+	sudo echo 'WantedBy=default.target' >> $copyq_service
+	sudo chmod 755 $copyq_service
 	sudo systemctl daemon-reload
 	sudo systemctl enable copyq.service
 	sudo systemctl start copyq.service
@@ -138,15 +149,15 @@ superfile_installation () {
 # alias and environment setup
 alias_environment_installation () {
 	echo "\033[34m[!] Setting Desktop Directories and alias \033[0m"
-	export PATH="/home/kali/.local/bin:$PATH"
+	export PATH="$HOME/.local/bin:$PATH"
 
-	mkdir /home/kali/Desktop/tool
-	mkdir /home/kali/Desktop/htb
-	mkdir /home/kali/Desktop/assessment
+	mkdir $HOME/Desktop/tool
+	mkdir $HOME/Desktop/htb
+	mkdir $HOME/Desktop/assessment
 
-	alias assessment="cd /home/kali/Desktop/assessment"
-	alias tool="cd /home/kali/Desktop/tool"
-	alias htb="cd /home/kali/Desktop/htb"
+	alias assessment="cd $HOME/Desktop/assessment"
+	alias tool="cd $HOME/Desktop/tool"
+	alias htb="cd $HOME/Desktop/htb"
 	alias hosts="sudo nano /etc/hosts"
 }
 
