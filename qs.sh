c78366242ad1ac3cc139931341f7f9b7b0d463dd
kaguramaru <92495243+kagura-maru@users.noreply.github.com>
2024-11-29 10:04:13 +0800

    Update qs.sh

--- qs.sh
+++ qs.sh
@@ -7,7 +7,7 @@ preparation () {
 	sudo apt-get update -y && sudo apt-get upgrade -y
 	sudo apt remove needrestart
 	echo "\033[34m[!] Installing CopyQ, Terminator Terminal and Sublime Gobuster \033[0m"
-	sudo apt-get install copyq terminator sublime-text gobuster seclists curl dnsrecon enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf nuclei python3-venv --fix-missing -y
+	sudo apt-get install copyq terminator sublime-text gobuster seclists curl dnsrecon enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf nuclei python3-venv locate --fix-missing -y
 }
 
 autorecon_installation () {
